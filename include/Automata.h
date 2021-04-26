25e5282181f1a3de92931d5d86b36441ea2ffa79
YuraNovichkov <80814495+YuraNovichkov@users.noreply.github.com>
2021-04-26 16:28:25 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,30 @@
+#pragma once
+const int N = 3;
+class Automata
+{
+	enum STATES
+	{
+		OFF,
+		WAIT,
+		ACCEPT,
+		CHECK,
+		COOK
+	};
+	unsigned int cash;
+	const char* menu[N] = { "Чай","Кофе","Молоко" };
+	const unsigned int price[N] = { 70,100,50 };
+	STATES state;
+
+	STATES getState();
+	bool check(unsigned int num);
+	void cook();
+	void finish();
+public:
+	Automata();
+	void on();
+	void off();
+	void coin(unsigned int);
+	const char** etMenu();
+	int choice(unsigned int);
+	int cancel();
+};
